8ec25bf99a8984cffcb00438eba3199e4198d91d
maheshshiva97 <maheshsiva9072@gmail.com>
2024-10-16 14:28:19 +0530

    script changes

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 // JavaScript for changing color when button is clicked
 document.getElementById("colorChangeBtn").addEventListener("click", function() {
-    document.body.style.backgroundColor = "#e74c3c"; // Change background to red
+    document.body.style.backgroundColor = "#e74c3c"||"#ed1132"; // Change background to red
 });
